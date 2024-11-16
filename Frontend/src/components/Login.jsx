8507237dc02834a61302eadaf6a9698d33e630a0
Nikita Sinha <103874407+nikita-sinha22@users.noreply.github.com>
2024-11-16 16:24:40 +0530

    Update Login.jsx

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -11,7 +11,7 @@ function Login() {
       email: data.email,
       password: data.password,
    };
-   await axios.post("/user/login",userInfo)
+   await axios.post("https://taleshelf-backend.onrender.com/user/login",userInfo)
    .then((res)=>{
       console.log(res.data);
       if(res.data){
