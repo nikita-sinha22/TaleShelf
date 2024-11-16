407404b9bffa94f98d6c257ab6516343df6bff36
Nikita Sinha <103874407+nikita-sinha22@users.noreply.github.com>
2024-11-16 18:01:51 +0530

    Update Freebook.jsx

--- Frontend/src/components/Freebook.jsx
+++ Frontend/src/components/Freebook.jsx
@@ -11,7 +11,7 @@ function Freebook() {
   useEffect(()=>{
      const getBook=async()=>{
         try{
-         const res= await axios.get("https://taleshelf-backend.onrender.com/book");
+         const res= await axios.get("http://localhost:4001/book");
          
          const data=res.data.filter((data)=>data.cateogry==="Free");
          console.log(data);
