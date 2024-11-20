d22aadc6abff70e18451f6d2d2771482ca38d163
Nikita Sinha <103874407+nikita-sinha22@users.noreply.github.com>
2024-11-20 15:01:33 +0530

    Update Navbar.jsx

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -44,7 +44,7 @@ function Navbar() {
       <a href='/'>Home</a>
     </li>
     <li>
-      <a href='/Course'>Course</a>
+      <a href='/course'>Course</a>
     </li>
     <li>
       <a>Contact</a>
